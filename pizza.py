fbb5c2330be119e54e108b3dd28a50cb632fc97f
heartwall-0 <1813763848@qq.com>
2021-02-13 22:21:22 +0800

    use of function

---
+++ pizza.py
@@ -0,0 +1,8 @@
+'''
+Author: your name
+Date: 2021-02-13 22:20:45
+LastEditTime: 2021-02-13 22:20:47
+LastEditors: Please set LastEditors
+Description: In User Settings Edit
+FilePath: \Python_modularity\pizza.py
+'''
